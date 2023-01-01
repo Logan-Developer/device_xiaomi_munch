265b7a306196f975405766d7d3291f665d4e7dc6
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-01-01 19:18:58 +0530

    munch: EvolutionX bringup
    Signed-off-by: Akash Kakkar <akash.galaxy07@gmail.com>
    Signed-off-by: Akash Kakkar <6381747+akash07k@users.noreply.github.com>

--- evolution_munch.mk
+++ evolution_munch.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/xiaomi/munch/device.mk)
@@ -21,7 +21,7 @@ endif
 
 TARGET_SUPPORTS_CALL_RECORDING := true
 
-PRODUCT_NAME := lineage_munch
+PRODUCT_NAME := evolution_munch
 PRODUCT_DEVICE := munch
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -30,3 +30,11 @@ PRODUCT_MODEL := POCO F4
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/munch/munch:13/RKQ1.211001.001/V14.0.1.0.TLMMIXM:user/release-keys
+
+# EvolutionX specific values
+EVO_BUILD_TYPE := OFFICIAL
+EVO_SIGNED := false
+TARGET_SUPPORTS_QUICK_TAP := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_CALL_RECORDING := true
