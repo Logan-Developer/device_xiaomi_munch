dda2b715ba9c88dca7bdd18a965f52171dabc440
Logan-Developer <logan2.humbert@gmail.com>
2023-01-03 23:52:51 +0100

    Revert "Munch: Add suport for MiuiCamera [1/2]"
    This reverts commit ea857a04ffe393aa6e2c770ff26d0152f590f10c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,11 +7,6 @@
 # Inherit from sm8250-common
 include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
-# Inherit from proprietary files for miuicamera
-include vendor/xiaomi/munch-miuicamera/products/board.mk
-
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
 # Firmware
 -include vendor/xiaomi-firmware/munch/BoardConfigVendor.mk
 
